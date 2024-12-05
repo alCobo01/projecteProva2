fe0f1537902e745e01b16933d9862e0e78c7540d
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2024-12-05 13:24:05 +0100

    Exericic 5 fet, closes #3

--- UtilsLibrary/Class1.cs
+++ UtilsLibrary/Class1.cs
@@ -1,7 +1,30 @@
 ﻿namespace UtilsLibrary
 {
-    public class Class1
+    public class Utils
     {
+        public static int[] BubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] < array[j])
+                    {
+                        int aux = array[i];
+                        array[i] = array[j];
+                        array[j] = aux;
+                    }
+                }
+            }
+            return array;
+        }
 
+        public static bool FindNumber(int[] array, int number) {
+            for (int i = 0; i < array.Length; i++) { 
+                if (array[i] == number) { return true; }
+            }
+            return false;
+        }
     }
+
 }
